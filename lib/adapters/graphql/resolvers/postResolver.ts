15f18812ac6a544b4e9a5b59db1bdfa15e89f2ef
João Milagres <joaoantoniomaruti@gmail.com>
2024-09-08 22:47:05 -0300

    Remove create post mutation

--- lib/adapters/graphql/resolvers/postResolver.ts
+++ lib/adapters/graphql/resolvers/postResolver.ts
@@ -4,18 +4,7 @@ const postResolver = {
       return [];
     },
   },
-  Mutation: {
-    createPost: (_parent: any, args: any) => {
-      return {
-        id: '1',
-        title: args.title,
-        author: args.author,
-        createdAt: new Date().toISOString(),
-        ups: 0,
-        comments: 0,
-      };
-    },
-  },
+  Mutation: {},
 };
 
 export default postResolver;
